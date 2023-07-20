7acdc724943bc477f3215da3aebb70db9ded6f19
Igaro-Franklin <igarofranklinr@gmail.com>
2023-07-20 09:27:26 -0300

    Teminando configuraçoes inicias da pagina de chamados

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom"
 import './dashboard.css'
 import { collection, getDocs, orderBy, limit, startAfter, query } from "firebase/firestore"
 import { db } from "../../services/firebaseConnection"
+import { format } from "date-fns"
 
 import Header  from '../../components/Header'
 import Title from '../../components/Title'
@@ -18,24 +19,27 @@ export default function Dashboard(){
     const [loading, setLoading] = useState(true);
     const [isEmpty, setIsEmpty] = useState(false)
 
-    useEffect ( () =>{
+    useEffect ( () => {
         async function loadChamados(){
             const q = query(listRef, orderBy('created', 'desc'), limit(5));
 
             const querySnapshot = await getDocs(q)
+            setChamados([]);
+
             await updateState(querySnapshot)
 
             setLoading(false);
         }
-        loadChamados()
 
-        return() =>{    }
+        loadChamados();
+
+        return() =>{}
     }, [])
 
     async function updateState(querySnapshot){
         const isCollectionEmpty = querySnapshot.size === 0;
 
-        if(isCollectionEmpty){
+        if(!isCollectionEmpty){
             let lista = [];
 
             querySnapshot.forEach((doc) =>{
@@ -45,6 +49,7 @@ export default function Dashboard(){
                     cliente: doc.data().cliente,
                     clienteId: doc.data().clienteId,
                     created: doc.data().created,
+                    createdFormat: format(doc.data().created.toDate(), "dd/MM/yyyy"),
                     status: doc.data().status,
                     complemento: doc.data().complemento,
                 })
@@ -54,6 +59,23 @@ export default function Dashboard(){
         }else{
             setIsEmpty(true)
         }
+    }
+
+    if(loading){
+        return(
+            <div>
+                <Header/>
+                <div className="content">
+                    <Title name="Tickets">
+                        <FiMessageSquare size={25}/>
+                    </Title>
+
+                    <div className="container dashboard">
+                        <span>Buscando chamado...</span>
+                    </div>
+                </div>
+            </div>
+        )
     }
 
     return(
@@ -92,24 +114,28 @@ export default function Dashboard(){
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td data-label='Cliente'>Mercado Esquina</td>
-                                        <td data-label='Assunto'>Suporte</td>
-                                        <td data-label='Status'>
-                                            <span className="badge" style={ {backgroundColor:"#999"} }>
-                                                Em aberto
-                                            </span>
-                                        </td>
-                                        <td data-label='Cadastrado'>18/07/2023</td>
-                                        <td data-label='#'>
-                                            <button className="action" style={ {backgroundColor:"#3588f6"} }>
-                                                <FiSearch  color="#fff" size={17} />
-                                            </button>
-                                            <button className="action"  size={17} style={ {backgroundColor:"#f6a935"} } >
-                                                <FiEdit2  color="#fff"/>
-                                            </button>
-                                        </td>
-                                    </tr>
+                                {chamados.map((item, index) =>{
+                                    return(
+                                        <tr key={index}>
+                                            <td data-label='Cliente'>{item.cliente}</td>
+                                            <td data-label='Assunto'>{item.assunto}</td>
+                                            <td data-label='Status'>
+                                                <span className="badge" style={ {backgroundColor:"#999"} }>
+                                                    {item.status}
+                                                </span>
+                                            </td>
+                                            <td data-label='Cadastrado'>{item.createdFormat}</td>
+                                            <td data-label='#'>
+                                                <button className="action" style={ {backgroundColor:"#3588f6"} }>
+                                                    <FiSearch  color="#fff" size={17} />
+                                                </button>
+                                                <button className="action"  size={17} style={ {backgroundColor:"#f6a935"} } >
+                                                    <FiEdit2  color="#fff"/>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
                                 </tbody>
                             </table>
 
